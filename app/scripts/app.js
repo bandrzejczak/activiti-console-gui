78789121f5a5ff2fa265cede82f8e83509ae87ef
Bartek Andrzejczak <ba.andrzejczak@gmail.com>
2014-09-28 18:27:31 +0200

    Initial scaffolded angular application

---
+++ app/scripts/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name activitiConsoleGuiApp
+ * @description
+ * # activitiConsoleGuiApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('activitiConsoleGuiApp', [
+    'ngCookies',
+    'ngResource',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name activitiConsoleGuiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the activitiConsoleGuiApp
+ */
+angular.module('activitiConsoleGuiApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
