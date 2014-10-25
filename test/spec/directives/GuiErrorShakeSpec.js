db525a7ab0907743da71c6f9d337f6a22a80ddb9
Bartek Andrzejczak <ba.andrzejczak@gmail.com>
2014-10-25 20:08:27 +0200

    Added guiErrorShake directive spec

---
+++ test/spec/directives/GuiErrorShakeSpec.js
@@ -0,0 +1,26 @@
+'use strict';
+
+describe('guiErrorShake', function() {
+
+    var element, scope;
+
+    beforeEach(module('activitiConsoleApp'));
+
+    beforeEach(inject(function($rootScope, $compile) {
+        scope = $rootScope.$new();
+        scope.error = undefined;
+        element = $compile('<div gui-error-shake error-msg="error"></div>')(scope);
+    }));
+
+    it('should add "shake" class when error variable changed', function () {
+        //given
+        element.attr('class', '');
+
+        //when
+        scope.error = "new error";
+        scope.$digest();
+
+        //then
+        expect(element.attr('class')).toBe('shake');
+    });
+});
