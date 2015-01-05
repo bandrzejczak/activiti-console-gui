bcc87b3305959bb305d112cdcfe1b0a74ccd2b03
Bartek Andrzejczak <ba.andrzejczak@gmail.com>
2015-01-05 18:05:23 +0100

    BGC-5 Create a menu

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,22 +2,30 @@
 
 /**
  * @ngdoc overview
- * @name activitiConsoleApp
+ * @name bpmConsoleApp
  * @description
- * # activitiConsoleApp
+ * # bpmConsoleApp
  *
  * Main module of the application.
  */
 angular
-    .module('activitiConsoleApp', [
+    .module('bpmConsoleApp', [
         'ngCookies',
         'ngResource',
         'ui.router',
+        'ui.bootstrap.dropdown',
         'ngAnimate',
-        'ngSanitize'
+        'ngSanitize',
+        'oitozero.ngSweetAlert'
     ])
     .config(function ($stateProvider, $urlRouterProvider) {
-        $urlRouterProvider.otherwise('/');
+        $urlRouterProvider.otherwise('/tasks/inbox');
+
+        var testController = function($scope, $http){
+            $scope.success = function(){
+                $http.post('api/deployment', $scope.file);
+            };
+        }
 
         $stateProvider
             .state('login', {
@@ -25,12 +33,79 @@ angular
                 templateUrl: 'views/login.html',
                 controller: 'LoginCtrl'
             })
-            .state('main', {
-                templateUrl: 'views/mainLayout.html'
+            .state('app', {
+                abstract: true,
+                templateUrl: 'views/mainLayout.html',
+                controller: function($scope, $state){
+                    $scope.stateIncludes = function(name){
+                        return $state.includes('app.'+name+'.*');
+                    }
+                }
             })
-            .state('main.root', {
-                url: '/',
-                templateUrl: 'views/home.html'
+            .state('app.tasks', {
+                abstract: true,
+                template: '<ui-view/>',
+                url: '/tasks'
+            })
+            .state('app.tasks.inbox', {
+                url: '/inbox',
+                templateUrl: 'views/home.html',
+                controller: testController
+            })
+            .state('app.tasks.awaiting', {
+                url: '/awaiting',
+                templateUrl: 'views/home.html',
+                controller: testController
+            })
+            .state('app.deployments', {
+                abstract: true,
+                template: '<ui-view/>',
+                url: '/deployments'
+            })
+            .state('app.deployments.add', {
+                url: '/add',
+                templateUrl: 'views/home.html',
+                controller: testController
+            })
+            .state('app.deployments.list', {
+                url: '/list',
+                templateUrl: 'views/home.html',
+                controller: testController
+            })
+            .state('app.processes', {
+                url: '/processes',
+                templateUrl: 'views/home.html',
+                controller: testController
+            })
+            .state('app.users', {
+                abstract: true,
+                template: '<ui-view/>',
+                url: '/users'
+            })
+            .state('app.users.add', {
+                url: '/add',
+                templateUrl: 'views/home.html',
+                controller: testController
+            })
+            .state('app.users.list', {
+                url: '/list',
+                templateUrl: 'views/home.html',
+                controller: testController
+            })
+            .state('app.groups', {
+                abstract: true,
+                template: '<ui-view/>',
+                url: '/groups'
+            })
+            .state('app.groups.add', {
+                url: '/add',
+                templateUrl: 'views/home.html',
+                controller: testController
+            })
+            .state('app.groups.list', {
+                url: '/list',
+                templateUrl: 'views/home.html',
+                controller: testController
             });
     })
     .value('language', {
